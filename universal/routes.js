674108ed37f69f942c9c45c10d8330faf8462833
x22a <ceo@stdio.digital>
2017-01-20 01:03:29 +0300

    A: stylelint

--- universal/routes.js
+++ universal/routes.js
@@ -1,13 +1,13 @@
 import React from 'react'/* global __DEV__:true */
 import { Route, IndexRoute } from 'react-router'
 
-import Main from './containers/Main'
+import MainContainer from './containers/Main'
 import SignUp from './containers/SignUp'
 import SignIn from './containers/SignIn'
 
 export default (
     <Route path='/'>
-        <IndexRoute component={ Main } />
+        <IndexRoute component={ MainContainer } />
         <Route path="sign-in" component={ SignIn } />
         <Route path="sign-up" component={ SignUp } />
     </Route>
