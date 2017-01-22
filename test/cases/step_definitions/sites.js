46465814f00d74be341b4d9dd1b08f9e6dd32377
x22a <hex22a@gmail.com>
2017-01-22 04:06:02 +0300

    impl

--- test/cases/step_definitions/sites.js
+++ test/cases/step_definitions/sites.js
@@ -2,13 +2,21 @@ import { graphql } from 'graphql'
 import schema from '../../../server/api/sites'
 
 const myStepDefinitionsWrapper = function stepDefinition() {
+    let site = null;
     this.When(/^User adds site with name: (.*), url: (.*), destinations: (.*)$/, async (name, url, rawDestinations) => {
         const destinations = rawDestinations.split(';');
 
         const query = `mutation AddSite{ addSite(name: "${name}", destinations: ${JSON.stringify(destinations)} url: "${url}"){ id, name, destinations, url } }`;
 
         const result = await graphql(schema, query);
-        console.log(result);
+        site = result.data.addSite;
+        console.log(site);
+    });
+
+    this.Then(/^User can get new site as an object$/, async () => {
+        if (!site || Object.keys(site).length === 0) {
+            throw new Error('No site added!');
+        }
     });
 };
 module.exports = myStepDefinitionsWrapper;
