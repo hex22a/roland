f836c0eb709429f3c36a9801646d85496ca46874
x22a <hex22a@gmail.com>
2017-01-28 05:05:49 +0300

    roland-2 Basic func unstable

--- test/cases/step_definitions/sites.js
+++ test/cases/step_definitions/sites.js
@@ -1,16 +1,35 @@
 import { graphql } from 'graphql'
 import schema from '../../../server/api/sites'
+import request from 'request'
+
 
 const myStepDefinitionsWrapper = function stepDefinition() {
     let site = null;
     this.When(/^User adds site with name: (.*), url: (.*), destinations: (.*)$/, async (name, url, rawDestinations) => {
         const destinations = rawDestinations.split(';');
 
-        const query = `mutation AddSite{ addSite(name: "${name}", destinations: ${JSON.stringify(destinations)} url: "${url}"){ id, name, destinations, url } }`;
+        const operationName = 'AddSite';
+        const query = `mutation ${operationName} { addSite(name: "${name}", destinations: ${JSON.stringify(destinations)} url: "${url}"){ id, name, destinations, url, foo } }`;
 
-        const result = await graphql(schema, query);
-        site = result.data.addSite;
-        console.log(site);
+        const payload = { operationName, query};
+        console.log(payload);
+
+        await request({
+            method: 'post',
+            url: 'http://localhost:3000/graphql',
+            json: true,
+            headers: {
+                Authorization: 'fofofof'
+            },
+            body: payload
+        }, (error, response, body) => {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log(response);
+                console.log(body);
+            }
+        })
     });
 
     this.Then(/^User can get new site as an object$/, async () => {
